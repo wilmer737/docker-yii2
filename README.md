1c847dd352f6d1a14fd6ac046e7161d090e35ac1
Wilmer <pollos737@outlook.com>
2017-10-15 15:28:27 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Docker Development Environment
+
+## Work in Progress
+
+
+### Includes:
+- PHP 7.1
+- Nginx
+- MySQL
+- Adminer
+
+Other included tech is: Semantic UI, Yii2 Framework, jQuery
---
+++ views/site/index.php
@@ -0,0 +1,23 @@
+<?php
+/** index. */
+
+/**
+ * @var \yii\web\View $this
+ */
+
+$this->title = "Yii2 Docker Dev Environment";
+
+?>
+<div class="ui segment very padded">
+    <h1>Work in Progress</h1>
+    <h2>Docker Development Environment</h2>
+    <span>Includes: </span>
+    <ul>
+        <li>PHP 7.1</li>
+        <li>Nginx</li>
+        <li>MySQL</li>
+        <li>Adminer</li>
+    </ul>
+    
+    Other included tech is: Semantic UI, Yii2 Framework, jQuery
+</div>
